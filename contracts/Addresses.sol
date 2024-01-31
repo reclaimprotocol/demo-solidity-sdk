71f6316154f5e6c8634dcda415c4e32ed690b514
TheLedgerOfJoudi <hadi.mohammad.saleh1@gmail.com>
2024-01-31 13:19:18 +0300

    prepare contracts for publish

---
+++ contracts/Addresses.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+/**
+ * Reclaim's addresses
+ */
+
+library Addresses {
+    address public constant BASE_MAINNET =
+        0xACE04E6DeB9567C1B8F37D113F2Da9E690Fc128d;
+    address public constant BNB_MAINNET =
+        0x1bbb0896aC6F4E32a89157C73Fb830325a441cb9;
+    address public constant POLYGON_MAINNET =
+        0x1bbb0896aC6F4E32a89157C73Fb830325a441cb9;
+    address public constant ARBITRUM_MAINNET =
+        0x1bbb0896aC6F4E32a89157C73Fb830325a441cb9;
+    address public constant OPTIMISM_MAINNET =
+        0x1bbb0896aC6F4E32a89157C73Fb830325a441cb9;
+    address public constant OPTIMISM_GOERLI_TESTNET =
+        0x2f0306D698685BEf7ff50745BFF7178F0d117ba8;
+    address public constant LINEA_TESTNET =
+        0x2f0306D698685BEf7ff50745BFF7178F0d117ba8;
+    address public constant PLOYGON_MUMBAI_TESTNET =
+        0x2f0306D698685BEf7ff50745BFF7178F0d117ba8;
+}
